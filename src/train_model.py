2589ddea112e78983d2c41e4ae0f2a0105144be2
Abhishek Maharana <119747866+Abhimaharana@users.noreply.github.com>
2025-11-24 12:55:46 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,32 @@
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, length
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+spark = SparkSession.builder.appName("Disaster_Response_LogisticRegression").getOrCreate()
+
+data = spark.read.csv("data/natural_disasters_2024.csv", header=True, inferSchema=True)
+
+data.show(5)
+data.printSchema()
+
+indexer = StringIndexer(inputCol="Disaster_Type", outputCol="label")
+data = indexer.fit(data).transform(data)
+
+feature_columns = ["Magnitude", "Fatalities", "Economic_Loss($)"]
+assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
+data = assembler.transform(data)
+
+train, test = data.randomSplit([0.8, 0.2], seed=42)
+
+lr = LogisticRegression(featuresCol="features", labelCol="label")
+lr_model = lr.fit(train)
+
+predictions = lr_model.transform(test)
+predictions.select("label", "prediction", "probability").show(10, truncate=False)
+
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Model Accuracy:", round(accuracy * 100, 2), "%")
